8f644c0fcc94bf9585eb7319831a8f18c20c4cbb
2439905184 <suchenglong018@126.com>
2022-02-25 16:07:06 +0800

    完成了客户端向服务器发送消息,服务端需要从命令行php server.php这样启动

--- src/server.php
+++ src/server.php
@@ -12,43 +12,6 @@
     <h2>服务器返回</h2>
 
     <script>
-   /* var ws = new WebSocket("ws:///websocket:1234/server.php")
-    console.log(ws)
-    ws.onopen = function(evt)
-    {
-        console.log("连接被打开")
-        console.log(ws.readyState)
-        alert("数据发送中")
-        //发送消息到服务端
-        //ws.send("hello world")
-       /* console.log(v.dateFormat(new Date(),"yyyy-MM-dd hh:mm:ss:S") + " onopen=" +  event);
-        v.timer = window.setInterval(function(){ //每隔5秒钟发送一次心跳，避免websocket连接因超时而自动断开
-            console.log('每隔5秒钟发送一次心跳');
-            var ping = {"type":"ping"};
-            v.webSocket.send(JSON.stringify(ping));
-        },5000);
-    }
-    ws.onmessage = function(evt)
-    {
-        console.log("收到信息"+evt.data)
-        //ws.close()
-    }
-    ws.onclose = function(evt)
-    {
-        console.log("连接被关闭")
-        console.log(evt.data)
-    }
-    //客户端发送给服务器
-    function client_send()
-    {
-        ws.send("hello")
-        //var msg = document.getElementById("")
-    }
-    //服务器发送给客户端
-    function sever_send()
-    {
-
-    }*/
     </script>
 </body>
 </html>
@@ -57,7 +20,9 @@
 //配置完composer.json之后需要使用composer dump-autoload手动更新autoload
 use Ratchet\Server\IoServer;
 use MyApp\Chat;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
 require dirname(__DIR__) . '/vendor/autoload.php';
-$server = IoServer::factory(new Chat(),8080);
+$server = IoServer::factory(new HttpServer(new WsServer(new Chat())),8080);
 $server->run();
 ?>
